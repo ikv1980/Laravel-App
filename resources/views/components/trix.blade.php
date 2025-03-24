39a7cd1d16e2f236468d5397f904fa60f71c1ad5
Konstantin <ikv1980@gmail.com>
2025-03-24 13:30:50 +0300

    Обучение: Trix разместил локально

--- resources/views/components/trix.blade.php
+++ resources/views/components/trix.blade.php
@@ -2,10 +2,19 @@
 <trix-editor input="{{$name}}" style="min-height: 200px;"></trix-editor>
 
 @once
+    {{--Файлы расположены через npm CDN--}}
+{{--    @push('css')--}}
+{{--        <link rel="stylesheet" type="text/css" href="{{asset('css/trix.css')}}">--}}
+{{--    @endpush--}}
+{{--    @push('js')--}}
+{{--        <script type="text/javascript" src="https://unpkg.com/trix@2.0.8/dist/trix.umd.min.js"></script>--}}
+{{--    @endpush--}}
+
+    {{--Файлы расположены локально--}}
     @push('css')
-        <link rel="stylesheet" type="text/css" href="https://unpkg.com/trix@2.0.8/dist/trix.css">
+        <link rel="stylesheet" type="text/css" href="{{asset('css/trix.css')}}">
     @endpush
     @push('js')
-        <script type="text/javascript" src="https://unpkg.com/trix@2.0.8/dist/trix.umd.min.js"></script>
+        <script type="text/javascript" src="{{asset('js/trix.js')}}"></script>
     @endpush
 @endonce
