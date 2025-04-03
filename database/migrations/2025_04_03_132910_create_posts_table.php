c02285b235319727a2cd292dc834f1b2f6648be9
Konstantin <ikv1980@gmail.com>
2025-04-03 16:54:52 +0300

    Обучение: Работа с миграциями

---
+++ database/migrations/2025_04_03_132910_create_posts_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id()->from(1001);
+            $table->timestamps();
+
+            //$table->bigInteger('user_id')->unsigned()->nullable();
+            //$table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->foreignId('user_id')->constrained('users');
+            $table->string('title');
+            $table->text('content');
+
+            $table->boolean('published')->default(true);
+            $table->timestamp('published_at')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('posts');
+    }
+};
